a1aa5e975f325f29df528f024e3befad0c2b3669
Dayne Broderson <dayne@alaska.edu>
2016-09-10 11:24:30 -0800

    bootstrap script

---
+++ bootstrap.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+sudo apt-get update
+sudo apt-get install python-imaging python-gdata python-imaging-tk
+
+#sudo pip install --upgrade google-api-python-client
+#sudo apt-get install luakit
+#sudo update-alternatives --configure x-www-browser
+
+
